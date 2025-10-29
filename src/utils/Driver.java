86d390086d9f0d02add0e2f9eef99e94172e16d0
CojoDani <cojo.daniel@yahoo.com>
2025-10-29 14:42:36 +0200

    New Project called Alarm

---
+++ src/utils/Driver.java
@@ -0,0 +1,21 @@
+package utils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    public static WebDriver driver;
+
+    public Driver(){
+        if(driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://www.alarm.com");
+        }
+    }
+
+    public static void closeThePage(){
+        driver.quit();
+    }
+
+
+}
